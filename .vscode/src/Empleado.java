4825b624ca75b78c8860a562a194b01506aee205
Daniel Jurado <daniele.juradoc@uqvirtual.edu.co>
2023-08-30 09:09:15 -0500

    recorte de lo innecesario

--- .vscode/src/Empleado.java
+++ .vscode/src/Empleado.java
@@ -3,18 +3,15 @@ public class Empleado extends persona{
     private String cargo;
 
     //--------------------------------------------------------------------------------------------------------------------------------------------
-
-    
-
+  public Empleado(String nombre, String id, String celular, String correo, String direccion, String cargo) {
+        super(nombre, id, celular, correo, direccion);
+        this.cargo = cargo;
+    }
     //--------------------------------------------------------------------------------------------------------------------------------------------
      
     public String getCargo(){
 
         return cargo;
-    }
-    public Empleado(String nombre, String id, String celular, String correo, String direccion, String cargo) {
-        super(nombre, id, celular, correo, direccion);
-        this.cargo = cargo;
     }
     public void setCargo (String nuevoCargo){
         this.cargo=nuevoCargo;
