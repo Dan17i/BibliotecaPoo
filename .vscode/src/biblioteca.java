bb354a855bbe1e7027a88ebcfc71af16db8647f3
Daniel Jurado <daniele.juradoc@uqvirtual.edu.co>
2023-08-30 17:45:47 -0500

    arraylist

--- .vscode/src/biblioteca.java
+++ .vscode/src/biblioteca.java
@@ -1,8 +1,13 @@
+import java.util.ArrayList;
+
 public class Biblioteca {
     public static void main(String[] args) {
         
-        
+       ArrayList<Libro>listalibros; 
+       ArrayList<Estudiante>listEstudiantes;
+       ArrayList<Empleado>listaEmpleados;
       
+       
 
     }
 }
