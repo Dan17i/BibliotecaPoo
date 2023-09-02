9e578eba948360ba0395c7d6e8fe3a2c52427de2
Daniel Jurado <daniele.juradoc@uqvirtual.edu.co>
2023-09-01 23:07:46 -0500

    variar
    no lo se pero hay está

--- .vscode/src/Empleado.java
+++ .vscode/src/Empleado.java
@@ -1,14 +1,12 @@
 public class Empleado extends persona{
-    
-    private String cargo;
 
+    private String cargo;
     //--------------------------------------------------------------------------------------------------------------------------------------------
   public Empleado(String nombre, String id, String celular, String correo, String direccion, String cargo) {
         super(nombre, id, celular, correo, direccion);
         this.cargo = cargo;
     }
     //--------------------------------------------------------------------------------------------------------------------------------------------
-     
     public String getCargo(){
 
         return cargo;
